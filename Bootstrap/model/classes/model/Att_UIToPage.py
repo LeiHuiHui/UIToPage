aada5c7344bd01b9b637651595d0e4477c2e3238
LeiHui <894556971@qq.com>
2019-03-28 12:21:36 +0800

    feature ndim4->ndim3

--- Bootstrap/model/classes/model/Att_UIToPage.py
+++ Bootstrap/model/classes/model/Att_UIToPage.py
@@ -62,12 +62,12 @@ class Att_UIToPage(AModel):
 
         visual_input = Input(shape=input_shape)
         learned_image_features = image_model(visual_input)
-        learned_image_features = np.array(learned_image_features)
-        learned_image_features = K.variable(value=learned_image_features,dtype='float32')
+        # learned_image_features = np.array(learned_image_features)
+        # learned_image_features = K.variable(value=learned_image_features,dtype='float32')
         print(image_model.output_shape)
         print(K.ndim(learned_image_features))
         print(K.shape(learned_image_features))
-        K.reshape(learned_image_features,(14,14,512))
+        K.reshape(learned_image_features,(3,14,14,512))
         print(K.ndim(learned_image_features))
         print(K.shape(learned_image_features))
 
