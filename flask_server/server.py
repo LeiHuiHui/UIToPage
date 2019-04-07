38095a2bf418d2a662767dd923174e652f74f2e2
LeiHui <894556971@qq.com>
2019-04-07 16:36:49 +0800

    OOM

--- flask_server/server.py
+++ flask_server/server.py
@@ -43,4 +43,6 @@ def sample():
 
 
 if __name__ == '__main__':
-    app.run(host="",port=8000,debug=True)
+    # app.run(host="",port=8000,debug=True)
+    app.run(host="0.0.0.0",port=8000,debug=True)
+
